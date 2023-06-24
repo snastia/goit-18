e1e39d21ec6b0244f374f819f02373361d06e8b4
Настя Скрицька <snastia.m@gmail.com>
2023-06-24 13:07:02 +0300

    гршргр

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { nanoid } from "nanoid";
+import { pnotify } from "pnotify";
+
+const id = nanoid()
+const id1 = nanoid()
+
+console.log(id)
+console.log(id1)
+
+const idy = pnotify()
+
+console.log(idy)
